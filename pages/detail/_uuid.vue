6697a27ea637ac748fb7333cb819d0c65ef8db22
wangfujie <784727590@qq.com>
2019-06-28 17:25:29 +0800

    增加百度自动推送screipt

--- pages/detail/_uuid.vue
+++ pages/detail/_uuid.vue
@@ -61,6 +61,21 @@
 </style>
 
 <script>
+(function(){
+    var bp = document.createElement('script');
+    var curProtocol = window.location.protocol.split(':')[0];
+    if (curProtocol === 'https') {
+        bp.src = 'https://zz.bdstatic.com/linksubmit/push.js';
+    }
+    else {
+        bp.src = 'http://push.zhanzhang.baidu.com/push.js';
+    }
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(bp, s);
+})();
+</script>
+
+<script>
 import axios from 'axios';
 import 'gitalk/dist/gitalk.css';
 import 'mavon-editor/dist/highlightjs/styles/tomorrow-night-eighties.min.css';
